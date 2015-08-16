65714b4b9fd65822bfe3d80bbf961a0f2096f1c5
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2015-08-16 16:59:59 -0700

    this is not working, flask now serves the angular app

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -8,10 +8,27 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function($scope, $upload, $location) {
+    $scope.onFileSelect = function($files) {
+		//$files: an array of files selected, each file has name, size, and type.
+		$scope.selectedFiles = $files;
+		$scope.progress = [];
+		for (var i = 0; i < $files.length; i++) {
+			var file = $files[i];
+			$scope.upload = $upload.upload({
+				url: '/api/uploadChat',
+				method: 'POST',
+				//headers: {'header-key': 'header-value'},
+				//withCredentials: true,
+				data: {},
+				file: file
+			}).progress(function(evt) {
+				$scope.progress[i-1] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+			}).success(function(data, status, headers, config) {
+				// file is uploaded successfully
+			}).then(function(){
+				$scope.selectedFiles = null;
+			});
+		}
+	}
+});
