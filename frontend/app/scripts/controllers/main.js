fbb7d03765275d194ca3c8eff731811fbf40bf4f
thomas0176 <ohet@gmx.de>
2015-08-17 06:39:46 +0000

    added sencond chart. is supposed to be a pie chart at some some point

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -28,13 +28,43 @@ angular.module('frontendApp')
 			}).success(function(data, status, headers, config) {
 				$scope.data = data;
 				var users = [];
-				var messages = []
+				var messages = [];
+				
+				//abs.NrOfMsg needed for PieChart
+				var absNrOfMsg = 0;
+				//get data for pie chart
+				var pieData =  [];
+				
+				
 				for (var key in data) {
   					if (data.hasOwnProperty(key)) {
-    					users.push(key);
-    					messages.push(data[key])
+    					users.push(key);	
+    					messages.push(data[key]);
+    					absNrOfMsg += data[key];
   					}
 				}
+			
+				
+				for (var key in data) {
+  					if (data.hasOwnProperty(key)) {
+  						var percentage = data[key]/absNrOfMsg*100;
+  						var n = percentage.toFixed(2);
+  						var percentageFixed = parseFloat(n);
+						
+  						var pieUser = 
+  						
+  						{
+			                name: key,
+			                y: percentageFixed
+        				 }
+    					pieData.push(pieUser);
+  					}
+				}
+				console.log(data);
+				console.log(messages);
+				console.log(pieData);
+
+				
 				$scope.highchartsNG = {
         			options: {
             			chart: {
@@ -52,6 +82,48 @@ angular.module('frontendApp')
         			},
         			loading: false
     				}
+    			
+    			
+    			$scope.highchartsNG2 = {
+        			
+            			chart: {
+            				plotBackgroundColor: null,
+				            plotBorderWidth: null,
+				            plotShadow: false,
+				            type: 'pie'
+            			},
+            			title: {
+        					text: 'Absolute message share per user'
+            			},
+            			
+            			tooltip: {
+				            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+				        },
+				        plotOptions: {
+				            pie: {
+				                allowPointSelect: true,
+				                cursor: 'pointer',
+				                dataLabels: {
+				                    enabled: true,
+				                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+				                    style: {
+				                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+				                    }
+				                }
+				            }
+				        },
+				        
+				        series: [{
+				        	name: "% of messages",
+				            colorByPoint: true,
+            				data: pieData
+            			
+        				}]
+        			
+    			}
+    				
+    				
+    				
 			}).then(function(){
 				$scope.selectedFiles = null;
 				$scope.fileUploaded = true;
