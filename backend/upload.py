5b02f4c2a2714c8f7255e5577857192b22669c73
Carlos Riquelme <rikel@Carloss-MacBook-Pro-3.local>
2015-08-19 20:35:01 -0700

    Checking Ramon's version.

--- backend/upload.py
+++ backend/upload.py
@@ -8,6 +8,11 @@ def upload_chat():
 	chat = Chat(StringIO(request.files['file'].read()))
 	users = [u.name for u in chat.users]
 	stats_chat = Statistics(chat)
-	charts = {'charts':[stats_chat.return_messages_by_user(as_chart=True),stats_chat.return_share_of_messages_by_user(as_chart=True),
-	stats_chat.return_number_of_messages_by_hour(as_chart=True),stats_chat.return_number_of_messages_by_hour_and_user(as_chart=True)]}
+	charts = {
+		'charts':[stats_chat.return_messages_by_user(as_chart=True),
+				  stats_chat.return_share_of_messages_by_user(as_chart=True),
+				  stats_chat.return_number_of_messages_by_hour(as_chart=True),
+				  stats_chat.return_number_of_messages_by_hour_and_user(as_chart=True),
+				  stats_chat.return_number_of_messages_by_week_and_year(as_chart=True)]
+			}
 	return jsonify(charts)
