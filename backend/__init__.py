1420ead693c88a6c9ced4ae52c6b9a8e0e4f7348
Ramon Iglesias <ramon.d.iglesias@gmail.com>
2015-08-26 20:40:18 -0700

    this fix allows developers to no longer run grunt build on every frontend change; that is, edits in source files are used to serve the static directories in flask; however, in production nginx still serves (and should always do) the minified files of the dist folder

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,9 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 from config import SQLALCHEMY_DATABASE_URI
 
-app = Flask('groupstats',static_url_path='',static_folder=os.getcwd()+'/frontend/dist')
+app = Flask('groupstats',static_url_path='',static_folder=os.getcwd()+'/frontend/app')
 app.config.update(
 	SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI,
 )
@@ -20,5 +20,9 @@ def test_route():
 @app.route('/')
 def send_index():
 	return app.send_static_file('index.html')
+
+@app.route('/bower_components/<path:filename>')
+def send_bower_components(filename):
+	return send_from_directory(os.getcwd()+'/frontend/bower_components/',filename)
 
 from backend import upload
